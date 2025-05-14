2f76c20062cfa07d539d318eb01eb823a5f081f7
Ritik_tanwar_01 <tanwarritik999@gmail.com>
2025-05-14 06:51:15 +0530

    debug2

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,5 +1,7 @@
+"use client"
 import Link from "next/link"
 import { Facebook, Twitter, Instagram, Youtube, Mail, Phone, MapPin } from "lucide-react"
+import { useEffect, useState } from "react"
 
 const Footer = () => {
   const fetchData = async () => {
