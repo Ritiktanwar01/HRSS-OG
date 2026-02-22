b86784746058e196f57d4bff3f5500cbf9572781
Ritik_tanwar_01 <tanwarritik999@gmail.com>
2026-02-23 00:16:40 +0530

    minor fix

--- app/(admin)/admin/gallery/page.jsx
+++ app/(admin)/admin/gallery/page.jsx
@@ -159,7 +159,7 @@ export default function AdminGalleryPage() {
       //   throw new Error("Authentication failed")
       // }
 
-      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/trust/upload`, {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/trust/upload/`, {
         method: "POST",
         headers: {
           Authorization: `Bearer ${localStorage.getItem("access_token")}`,
